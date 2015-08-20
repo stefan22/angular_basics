5d06e7d2dafa99fb8c08adcccec163ca10459b17
Stefan Lissia <stefan.emailacct@gmail.com>
2015-08-20 23:09:54 +0100

    forgot to link up css

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', function($scope) {
-  $scope.author = {
+  $scope.artist = {
     'name' : 'Jon Solong',
     'title': 'Staff member',
     'company' : 'Acer Corporation'
