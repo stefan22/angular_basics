df5b941d09fe16d45c8dca75e06673eacd3a5354
Stefan Lissia <stefan.emailacct@gmail.com>
2015-08-20 23:01:27 +0100

    adss css reset and images

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,9 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller("MyController", function($scope) {
-    $scope.author = {
-      'name' : 'John Doe',
-      'title': 'Staff',
-      'company' : 'Acer Corporation'
-    }
+myApp.controller('MyController', function($scope) {
+  $scope.author = {
+    'name' : 'Jon Solong',
+    'title': 'Staff member',
+    'company' : 'Acer Corporation'
+  }
 });
