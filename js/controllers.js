7d1b8a5ec435115734d97167d437fe48ad984847
Stefan Lissia <stefan.emailacct@gmail.com>
2015-08-21 15:59:23 +0100

    configs partials using route provider service

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,8 @@
 var artistControllers = angular.module('artistControllers', []);
 
-artistControllers.controller('ListController', function($scope, $http) {
-  $http.get('js/data.json').success(function(data) {
-    $scope.artists = data;
-    $scope.artistOrder = 'name';
-  });
-});
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+    $http.get('js/data.json').success(function(data) {
+        $scope.artists = data;
+        $scope.artistOrder = 'name';
+    });
+}]);
