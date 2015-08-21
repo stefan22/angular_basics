cfcafe6c364265fe53632c64ae15eca17968d1f2
Stefan Lissia <stefan.emailacct@gmail.com>
2015-08-21 13:21:24 +0100

    fixes a problem with form - initializes name in form at the controller

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyController', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
+    $scope.artistOrder = 'name';
   });
 });
