d92612afcd4e185f3eaad09fccbc9a9d88763247
Stefan Lissia <stefan.emailacct@gmail.com>
2015-08-22 17:35:02 +0100

    binding input fields through filters

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,7 @@ var myApp = angular.module('myApp', []);
 myApp.controller('ListController', ['$scope', '$http', function($scope,$http) {
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
+    $scope.artistOrder = 'name';
   });
 
 }]);
