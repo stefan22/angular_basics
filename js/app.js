4df2a5fd21f4188d0c13ab884639227277a8530d
Stefan Lissia <stefan.emailacct@gmail.com>
2015-08-23 23:49:55 +0100

    wiring second partial under app config

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ myApp.config(['$routeProvider', function($routeProvider) {
         templateUrl: 'partials/list.html',
         controller: 'ListController'
     }).
+    when('/details/:itemId', {
+        templateUrl: 'partials/details.html',
+        controller: 'DetailsController'
+    }).
     otherwise({
         redirectTo: '/list'
     });
