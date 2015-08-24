21adea4e74924152330d890ced9c318aa380209c
Stefan Lissia <stefan.emailacct@gmail.com>
2015-08-24 16:05:58 +0100

    finished up second partial

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ var myApp = angular.module('myApp', [
 
 myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-
     when('/list', {
         templateUrl: 'partials/list.html',
         controller: 'ListController'
--- js/controllers.js
+++ js/controllers.js
@@ -8,7 +8,7 @@ artistControllers.controller('ListController', ['$scope', '$http', function($sco
 }]);
 
 
-artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http) {
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
     $http.get('js/data.json').success(function(data) {
         $scope.artists = data;
         $scope.whichItem = $routeParams.itemId;
